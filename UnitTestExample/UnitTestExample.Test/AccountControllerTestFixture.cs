4a18f025b29171cb6b3b2888cf04116c361329a0
Márton Miklós Szamos <marton.szamos@stud.uni-corvinus.hu>
2021-11-26 17:33:32 +0100

    email függvény

---
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestExample.Test
+{
+    public class AccountControllerTestFixture
+    {
+        // Arrange
+        var accountController = new AccountController();
+
+        // Act
+        var actualResult = accountController.ValidateEmail(email);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
